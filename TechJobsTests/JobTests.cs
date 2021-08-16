23e0aa655347b90913e5db9d605b31fbaa0c71df
Tiah Keefe <tetered12@gmail.com>
2021-08-15 21:37:30 -0500

    Creating unit testing for JobId

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -1,13 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
 
 namespace TechJobsTests
 {
     [TestClass]
-    public class UnitTest1
+    public class JobTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSettingJobId()
         {
+            Job test_job1 = new Job();
+            Job test_job2 = new Job();
+            Assert.IsFalse(test_job2.Id-1 != test_job1.Id);
         }
     }
 }
