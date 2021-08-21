a74c9738dbe56602ab54096027c79e20cf54d75f
Tiah Keefe <tetered12@gmail.com>
2021-08-21 16:59:00 -0500

    Adding more TDD to make sure my Job class is running smoothly.

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -13,5 +13,31 @@ namespace TechJobsTests
             Job test_job2 = new Job();
             Assert.IsFalse(test_job2.Id-1 != test_job1.Id);
         }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            
+            Employer test_employerName = new Employer("ACME");
+            Location test_location = new Location("Desert");
+            PositionType test_position_type = new PositionType("Quality Control");
+            CoreCompetency test_core_competency = new CoreCompetency("Persistence");
+
+            Job test_job3 = new Job("Product tester", test_employerName, test_location, test_position_type, test_core_competency);
+            Assert.AreEqual("Product tester", test_job3.Name);
+            Assert.AreEqual(test_employerName, test_job3.EmployerName);
+            Assert.AreEqual(test_location, test_job3.EmployerLocation);
+            Assert.AreEqual(test_position_type, test_job3.JobType);
+            Assert.AreEqual(test_core_competency, test_job3.JobCoreCompetency);
+        }
+
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Job job_idTest1 = new Job();
+            Job job_idTest2 = new Job();
+            Assert.IsFalse(job_idTest1.Equals(job_idTest2));
+            
+        }
     }
 }
